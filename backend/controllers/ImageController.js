78afdc836857b0ca020b4431c1bf1e9047e45894
sahil25123 <sahilgupta25123@gmail.com>
2025-07-09 14:37:38 +0530

    Fix typos in variable names and improve formatting in removeBgImage function

--- backend/controllers/ImageController.js
+++ backend/controllers/ImageController.js
@@ -1,13 +1,11 @@
 import axios from "axiox";
 import fs from "fs";
 import FormData from "from-data";
-
 import User from "../models/user.js";
-
 
 const removeBgImage = async(req,res)=>{
     try{
-        const {clerkid}  = req.body;
+        const {clerkId}  = req.body;
         const user = await User.findOne({clerkId});
 
         if(!user) {
@@ -20,15 +18,23 @@ const removeBgImage = async(req,res)=>{
         }
 
         const imagePath = req.file.path;
-        // Reading the image file 
 
+        // Reading the image file 
         const imageFile = fs.createReadStream(imagePath);
         const formData = new formData();
 
-        
+        formData.append("image_file" , imageFile);
+        const {data} = await axios.post("https://clipdrop-api.co/remove-background/v1", formData , {
+            headers :{
+                "x-api-key" :process.env.CLIPDROP_API
+            } ,
+            responseType:"arraybuffer"
+        });
 
-
-
+        const base64Image = Buffer.from(data , "binary").toString("base64");
+        const resultImage = `data:${req.file.mimetype};base64,${base64Image}`
+        await User.findByIdAndUpdate(user._id,{creditBalancw :user.creditBalancw-1})
+        res.json({success:True , resultImage , creditBalancw:user.creditBalancw-1, messaage :"Backgroung Image Removed"})
     }
     catch(e){
         return res.json(500).send({success : false  ,message : e.message})
